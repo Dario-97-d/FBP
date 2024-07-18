826957e2cb366757896cf4422fc84c92d2e7afde
Dario-97-d <dariodias97@gmail.com>
2024-07-18 16:53:01 +0100

    Refactor js-references.php (code alignment)

--- frontend/js-references.php
+++ frontend/js-references.php
@@ -4,11 +4,11 @@
 	$_JSREF_api_is_username_available = 'scripts/api/is-username-available.js';
 	
 	// On Submit (event).
-	$_JSREF_on_submit_register   = 'scripts/on-submit-register.js';
-	$_JSREF_on_submit_search     = 'scripts/on-submit-search.js';
+	$_JSREF_on_submit_register        = 'scripts/on-submit-register.js';
+	$_JSREF_on_submit_search          = 'scripts/on-submit-search.js';
 	
 	// Validate.
-	$_JSREF_validate_email       = 'scripts/validate/validate-email.js';
-	$_JSREF_validate_password    = 'scripts/validate/validate-password.js';
-	$_JSREF_validate_player_name = 'scripts/validate/validate-player-name.js';
-	$_JSREF_validate_username    = 'scripts/validate/validate-username.js';
+	$_JSREF_validate_email            = 'scripts/validate/validate-email.js';
+	$_JSREF_validate_password         = 'scripts/validate/validate-password.js';
+	$_JSREF_validate_player_name      = 'scripts/validate/validate-player-name.js';
+	$_JSREF_validate_username         = 'scripts/validate/validate-username.js';
