8263755683ee897b11db982da478e0f65ff203ec
Dario-97-d <dariodias97@gmail.com>
2024-07-25 21:21:39 +0100

    Frontend: Use CSS Grid instead of HTML table for Team Applications

--- frontend/content-views/team/manage/view-team-manage-applications.php
+++ frontend/content-views/team/manage/view-team-manage-applications.php
@@ -1,60 +1,57 @@
 
 <h1><?= $_team_name ?></h1>
 
-<?php
+<h2>Applications</h2>
 
-	// If sql query for applicants was successful.
-	if ( isset( $_team_applicants ) )
-	{
-		?>
-		
-		<h2>Applications</h2>
-		<table class="table1" cellpadding="8">
-		
-			<?php
-			
-				if ( is_array( $_team_applicants ) )
-				{
-					foreach( $_team_applicants as $row )
-					{
-						?>
-						
-						<tr>
-							<td>
-								@<?= $row['username'] ?>
-							</td>
-							<td>
-								<a href="player-profile?id=<?= $row['player_id'] ?>"><?= $row['player_name'] ?></a>
-							</td>
-							<td>
-								<form method="POST" onsubmit="return confirm('Accept <?= $row['player_name'] ?>'s application?')">
-									<input type="hidden" name="accept-player-id" value="<?= $row['player_id'] ?>" />
-									<input type="submit" value="Accept" />
-								</form>
-							</td>
-							<td>
-								<form method="POST" onsubmit="return confirm('Reject <?= $row['player_name'] ?>'s application?')">
-									<input type="hidden" name="reject-player-id" value="<?= $row['player_id'] ?>" />
-									<input type="submit" value="Reject" />
-								</form>
-							</td>
-						</tr>
-						
-						<?php
-					}
-				}
-				else
-				{
-					?>
+<div class="display-team-applications">
+	
+	<?php
+	
+		if ( is_array( $_team_applicants ) )
+		{
+			foreach( $_team_applicants as $row )
+			{
+				?>
+				
+				<!-- Username -->
+				<div class="display-team-applications-body at-username">@<?= $row['username'] ?></div>
+				
+				<!-- Player name -->
+				<div class="display-team-applications-body">
+					<a href="player-profile?id=<?= $row['player_id'] ?>">
+						<?= $row['player_name'] ?>
+					</a>
+				</div>
+				
+				<!-- Options -->
+				<div class="display-team-applications-body">
 					
-					<tr><td>None</td></tr>
+					<!-- Accept Application -->
+					<form method="POST" onsubmit="return confirm('Accept application from <?= $row['player_name'] ?>?')">
+						<input type="hidden" name="accept-player-id" value="<?= $row['player_id'] ?>" />
+						<input type="submit" value="Accept" />
+					</form>
 					
-					<?php
-				}
-			
+					<!-- Reject Application -->
+					<form method="POST" onsubmit="return confirm('Reject application from <?= $row['player_name'] ?>?')">
+							<input type="hidden" name="reject-player-id" value="<?= $row['player_id'] ?>" />
+							<input type="submit" value="Reject" />
+						</form>
+					
+				</div>
+				
+				<?php
+			}
+		}
+		else
+		{
 			?>
 			
-		</table>
-		
-		<?php
-	}
+			<div class="display-team-applications-body grid-span-col-3">None</div>
+			
+			<?php
+		}
+	
+	?>
+	
+</div>
