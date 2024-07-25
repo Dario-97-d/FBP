43b5f849b1f639ebd8232f1f3023119159e8c337
Dario-97-d <dariodias97@gmail.com>
2024-07-25 21:47:45 +0100

    Frontend: Use CSS Grid instead of HTML table for Team Center

--- frontend/content-views/team/view-team-center.php
+++ frontend/content-views/team/view-team-center.php
@@ -1,106 +1,103 @@
 
 <h1>Team</h1>
 
-<?php
+<h2>Applications sent</h2>
 
-	// If sql query for teams applied to was successful.
-	if ( isset( $_team_applications ) )
-	{
-		?>
-		
-		<h2>Applications sent</h2>
-		
-		<table class="table1" cellpadding="8">
-		
-		<?php
-		
-			if ( is_array( $_team_applications ) )
-			{
-				foreach( $_team_applications as $row )
-				{
-					?>
-					
-					<tr>
-						<td><a href="team-profile?id=<?= $row['id'] ?>"><?= $row['team_name'] ?></a></td>
-						<td>
-							<form method="POST" onsubmit="return confirm('Cancel application to <?= $row['team_name'] ?>?')">
-								<input type="hidden" name="cancel-team-id" value="<?= $row['id'] ?>" />
-								<input type="submit" value="Cancel" />
-							</form>
-						</td>
-					</tr>
-					
-					<?php
-				}
-			}
-			else
+<div class="display-player-applications">
+	
+	<?php
+	
+		if ( is_array( $_team_applications ) )
+		{
+			foreach ( $_team_applications as $row )
 			{
 				?>
 				
-				<tr><td>None</td></tr>
+				<!-- Team -->
+				<div class="display-player-applications-body">
+					<a href="team-profile?id=<?= $row['id'] ?>">
+						<?= $row['team_name'] ?>
+					</a>
+				</div>
+				
+				<!-- Cancel Application -->
+				<div class="display-player-applications-body">
+					<form method="POST" onsubmit="return confirm('Cancel application to <?= $row['team_name'] ?>?')">
+						<input type="hidden" name="cancel-team-id" value="<?= $row['id'] ?>" />
+						<input type="submit" value="Cancel" />
+					</form>
+				</div>
 				
 				<?php
 			}
-		
-		?>
-			
-		</table>
-		
-		<?php
-	}
-	
-	// If sql query for invites was successful.
-	if ( isset( $_team_invites ) )
-	{
-		?>
-		
-		<h2>Invites</h2>
-		
-		<table class="table1" cellpadding="8">
-		
-			<?php
-			
-				if ( is_array( $_team_invites ) )
-				{
-					foreach( $_team_invites as $row )
-					{
-						?>
-						
-						<tr>
-							<td><a href="team-profile?id=<?= $row['id'] ?>"><?= $row['team_name'] ?></a></td>
-							<td>
-								<form method="POST" onsubmit="return confirm('Accept invite to <?= $row['team_name'] ?>?')">
-									<input type="hidden" name="accept-team-id" value="<?= $row['id'] ?>" />
-									<input type="submit" value="Accept" />
-								</form>
-							</td>
-							<td>
-								<form method="POST" onsubmit="return confirm('Reject invite to <?= $row['team_name'] ?>?')">
-									<input type="hidden" name="reject-team-id" value="<?= $row['id'] ?>" />
-									<input type="submit" value="Reject" />
-								</form>
-							</td>
-						</tr>
-						
-						<?php
-					}
-				}
-				else
-				{
-					?>
-					
-					<tr><td>None</td></tr>
-					
-					<?php
-				}
-			
+		}
+		else
+		{
 			?>
 			
-		</table>
-		
-		<?php
-	}
+			<div class="display-player-applications-body grid-span-col-2">None</div>
+			
+			<?php
+		}
 	
+	?>
+	
+</div>
+
+<h2>Invites</h2>
+
+<div class="display-player-invites">
+
+	<?php
+	
+		if ( is_array( $_team_invites ) )
+		{
+			foreach( $_team_invites as $row )
+			{
+				?>
+				
+				<!-- Team -->
+				<div class="display-player-invites-body">
+					<a href="team-profile?id=<?= $row['id'] ?>">
+						<?= $row['team_name'] ?>
+					</a>
+				</div>
+				
+				<!-- Options -->
+				<div class="display-player-invites-body">
+					
+					<!-- Accept Invite -->
+					<form method="POST" onsubmit="return confirm('Accept invite to <?= $row['team_name'] ?>?')">
+						<input type="hidden" name="accept-team-id" value="<?= $row['id'] ?>" />
+						<input type="submit" value="Accept" />
+					</form>
+					
+					<!-- Reject Invite -->
+					<form method="POST" onsubmit="return confirm('Reject invite to <?= $row['team_name'] ?>?')">
+						<input type="hidden" name="reject-team-id" value="<?= $row['id'] ?>" />
+						<input type="submit" value="Reject" />
+					</form>
+					
+				</div>
+				
+				<?php
+			}
+		}
+		else
+		{
+			?>
+			
+			<div class="display-player-invites-body grid-span-col-2">None</div>
+			
+			<?php
+		}
+	
+	?>
+	
+</div>
+		
+<?php
+
 	// Show button leave team OR links to find/create team.
 	if ( $_player_has_team )
 	{
