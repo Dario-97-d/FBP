99e1fed374cb31ad28e8c72604593ff281979fac
Dario-97-d <dariodias97@gmail.com>
2024-07-25 21:32:56 +0100

    Frontend: Use CSS Grid instead of HTML table for Team Invites

--- frontend/content-views/team/manage/view-team-manage-invites.php
+++ frontend/content-views/team/manage/view-team-manage-invites.php
@@ -8,54 +8,48 @@
 	require_once $_FILEREF_partial_view_search_player;
 ?>
 
-<?php
+<h2>Invites</h2>
 
-	// If sql query for applicants was successful.
-	if ( isset( $_invitees ) )
-	{
-		?>
-		
-		<h2>Invitations</h2>
-		<table class="table1" cellpadding="8">
-		
-			<?php
-			
-				if ( is_array( $_invitees ) )
-				{
-					foreach( $_invitees as $row )
-					{
-						?>
-						
-						<tr>
-							<td>
-								@<?= $row['username'] ?>
-							</td>
-							<td>
-								<a href="player-profile?id=<?= $row['player_id'] ?>"><?= $row['player_name'] ?></a>
-							</td>
-							<td>
-								<form method="POST" onsubmit="return confirm('Withdraw invite to <?= $row['player_name'] ?>?')">
-									<input type="hidden" name="withdraw-player-id" value="<?= $row['player_id'] ?>" />
-									<input type="submit" value="Withdraw" />
-								</form>
-							</td>
-						</tr>
-						
-						<?php
-					}
-				}
-				else
-				{
-					?>
-					
-					<tr><td>None</td></tr>
-					
-					<?php
-				}
-			
+<div class="display-team-invites">
+	
+	<?php
+	
+		if ( is_array( $_invitees ) )
+		{
+			foreach ( $_invitees as $row )
+			{
+				?>
+				
+				<!-- Username -->
+				<div class="display-team-invites-body at-username">@<?= $row['username'] ?></div>
+				
+				<!-- Player name -->
+				<div class="display-team-invites-body">
+					<a href="player-profile?id=<?= $row['player_id'] ?>">
+						<?= $row['player_name'] ?>
+					</a>
+				</div>
+				
+				<!-- Withdraw Invite -->
+				<div class="display-team-invites-body">
+					<form method="POST" onsubmit="return confirm('Withdraw invite to <?= $row['player_name'] ?>?')">
+						<input type="hidden" name="withdraw-player-id" value="<?= $row['player_id'] ?>" />
+						<input type="submit" value="Withdraw" />
+					</form>
+				</div>
+				
+				<?php
+			}
+		}
+		else
+		{
 			?>
 			
-		</table>
-		
-		<?php
-	}
+			<div class="display-team-invites-body grid-span-col-3">None</div>
+			
+			<?php
+		}
+	
+	?>
+	
+</div>
