788720a268ecf58003ed1cbb5453daf95cbfb0f6
Dario-97-d <dariodias97@gmail.com>
2024-08-02 11:01:15 +0100

    Refactor: Align code in file-references.php

--- backend/file-references.php
+++ backend/file-references.php
@@ -3,51 +3,51 @@
 	// -- Backend --
 	
 	// General functions.
-	$_FILEREF_logger_functions           = __DIR__ . '/../functions/logger-functions.php';
-	$_FILEREF_result_functions           = __DIR__ . '/../functions/result-functions.php';
-	$_FILEREF_sql_functions              = __DIR__ . '/../functions/sql-functions.php';
+	$_FILEREF_logger_functions                = __DIR__ . '/../functions/logger-functions.php';
+	$_FILEREF_result_functions                = __DIR__ . '/../functions/result-functions.php';
+	$_FILEREF_sql_functions                   = __DIR__ . '/../functions/sql-functions.php';
 	
 	// Request functions.
-	$_FILEREF_auth_functions             = __DIR__ . '/../functions/request/auth-functions.php';
-	$_FILEREF_input_handling_functions   = __DIR__ . '/../functions/request/input-handling-functions.php';
-	$_FILEREF_url_functions              = __DIR__ . '/../functions/request/url-functions.php';
+	$_FILEREF_auth_functions                  = __DIR__ . '/../functions/request/auth-functions.php';
+	$_FILEREF_input_handling_functions        = __DIR__ . '/../functions/request/input-handling-functions.php';
+	$_FILEREF_url_functions                   = __DIR__ . '/../functions/request/url-functions.php';
 	
 	// Response functions.
-	$_FILEREF_dialog_functions           = __DIR__ . '/../functions/response/dialog-functions.php';
-	$_FILEREF_die_functions              = __DIR__ . '/../functions/response/die-functions.php';
-	$_FILEREF_redirect_functions         = __DIR__ . '/../functions/response/redirect-functions.php';
+	$_FILEREF_dialog_functions                = __DIR__ . '/../functions/response/dialog-functions.php';
+	$_FILEREF_die_functions                   = __DIR__ . '/../functions/response/die-functions.php';
+	$_FILEREF_redirect_functions              = __DIR__ . '/../functions/response/redirect-functions.php';
 	
 	// Feature functions.
-	$_FILEREF_mail_functions             = __DIR__ . '/../functions/features/mail-functions.php';
-	$_FILEREF_mates_functions            = __DIR__ . '/../functions/features/mates-functions.php';
-	$_FILEREF_player_functions           = __DIR__ . '/../functions/features/player-functions.php';
-	$_FILEREF_search_functions           = __DIR__ . '/../functions/features/search-functions.php';
-	$_FILEREF_team_functions             = __DIR__ . '/../functions/features/team-functions.php';
-	$_FILEREF_user_functions             = __DIR__ . '/../functions/features/user-functions.php';
+	$_FILEREF_mail_functions                  = __DIR__ . '/../functions/features/mail-functions.php';
+	$_FILEREF_mates_functions                 = __DIR__ . '/../functions/features/mates-functions.php';
+	$_FILEREF_player_functions                = __DIR__ . '/../functions/features/player-functions.php';
+	$_FILEREF_search_functions                = __DIR__ . '/../functions/features/search-functions.php';
+	$_FILEREF_team_functions                  = __DIR__ . '/../functions/features/team-functions.php';
+	$_FILEREF_user_functions                  = __DIR__ . '/../functions/features/user-functions.php';
 	
 	// Game functions.
-	$_FILEREF_play_3_functions           = __DIR__ . '/../functions/game/play-3-functions.php';
+	$_FILEREF_play_3_functions                = __DIR__ . '/../functions/game/play-3-functions.php';
 	
 	// -- Frontend --
 	
 	// Functions.
-	$_FILEREF_js_functions               = __DIR__ . '/../functions/frontend/js-functions.php';
-	$_FILEREF_page_functions             = __DIR__ . '/../functions/frontend/page-functions.php';
+	$_FILEREF_js_functions                    = __DIR__ . '/../functions/frontend/js-functions.php';
+	$_FILEREF_page_functions                  = __DIR__ . '/../functions/frontend/page-functions.php';
 	
 	// JS scripts references.
-	$_FILEREF_js_references              = __DIR__ . '/../frontend/js-references.php';
+	$_FILEREF_js_references                   = __DIR__ . '/../frontend/js-references.php';
 	
 	// Layout and Content files.
-	$_FILEREF_frontend_content           = __DIR__ . '/../frontend/content.php';
-	$_FILEREF_frontend_layout            = __DIR__ . '/../frontend/layout.php';
+	$_FILEREF_frontend_content                = __DIR__ . '/../frontend/content.php';
+	$_FILEREF_frontend_layout                 = __DIR__ . '/../frontend/layout.php';
 	
 	// Partial Views.
 	$_FILEREF_partial_view_generic_attributes = __DIR__ . '/../frontend/partial-views/partial-view-generic-attributes.php';
-	$_FILEREF_partial_view_logout_link   = __DIR__ . '/../frontend/partial-views/logout-link.html';
-	$_FILEREF_partial_view_search_player = __DIR__ . '/../frontend/partial-views/search-player.html';
-	$_FILEREF_partial_view_search_team   = __DIR__ . '/../frontend/partial-views/search-team.html';
-	$_FILEREF_partial_view_top_bar       = __DIR__ . '/../frontend/partial-views/top-bar.html';
-	$_FILEREF_partial_view_website_map   = __DIR__ . '/../frontend/partial-views/website-map.html';
+	$_FILEREF_partial_view_logout_link        = __DIR__ . '/../frontend/partial-views/logout-link.html';
+	$_FILEREF_partial_view_search_player      = __DIR__ . '/../frontend/partial-views/search-player.html';
+	$_FILEREF_partial_view_search_team        = __DIR__ . '/../frontend/partial-views/search-team.html';
+	$_FILEREF_partial_view_top_bar            = __DIR__ . '/../frontend/partial-views/top-bar.html';
+	$_FILEREF_partial_view_website_map        = __DIR__ . '/../frontend/partial-views/website-map.html';
 	
 	// View Error.
-	$_FILEREF_view_error                 = __DIR__ . '/../frontend/content-views/view-error.php';
+	$_FILEREF_view_error                      = __DIR__ . '/../frontend/content-views/view-error.php';
